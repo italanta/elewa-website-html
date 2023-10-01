bce84a22411e5f2b3c959c7e4e0ff9376a9892a1
jobukk <jobukipkurui3843@gmail.com>
2023-10-02 00:04:24 +0300

    :zap: making the hurmbarger menu functional with script

--- src/features/pages/script.js
+++ src/features/pages/script.js
@@ -1,13 +1,15 @@
 
-
+// selectors
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
 
+// event listner then using toggle()
 hamburger.addEventListener("click",()=>{
     hamburger.classList.toggle("active");
     navMenu.classList.toggle("active");
 })
 
+//selecting each link in the navbar 
 document.querySelectorAll(".nav-item").forEach(n => n.addEventListener("click", () =>{
     navMenu.classList.remove("active");
     hamburger.classList.remove("active");
