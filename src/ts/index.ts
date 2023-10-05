6a4eae51a25c890e8dea260d72bf797a2ffd4e00
+ <+>
2023-10-05 12:06:00 +0300

    :lipstick:created Website-header

---
+++ src/ts/index.ts
@@ -0,0 +1,15 @@
+// document.addEventListener("DOMContentLoaded", function () {
+//     const solutionsTab = document.querySelector(".solution");
+//     const solutionMenu = document.querySelector(".menu");
+
+//     solutionsTab.addEventListener("click", function () {
+//         solutionMenu.classList.toggle("open");
+//     });
+
+//     // Close the menu if you click outside of it
+//     document.addEventListener("click", function (event) {
+//         if (!solutionMenu.contains(event.target) && event.target !== solutionsTab) {
+//             solutionMenu.classList.remove("open");
+//         }
+//     });
+// });
