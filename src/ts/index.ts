ddcddbf129b24244d79af4eae6bfe98423125215
+ <+>
2023-10-05 14:43:42 +0300

    :lipstick:created a Website-header-#1

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,15 +1,22 @@
-// document.addEventListener("DOMContentLoaded", function () {
-//     const solutionsTab = document.querySelector(".solution");
-//     const solutionMenu = document.querySelector(".menu");
-
-//     solutionsTab.addEventListener("click", function () {
-//         solutionMenu.classList.toggle("open");
-//     });
-
-//     // Close the menu if you click outside of it
-//     document.addEventListener("click", function (event) {
-//         if (!solutionMenu.contains(event.target) && event.target !== solutionsTab) {
-//             solutionMenu.classList.remove("open");
-//         }
-//     });
-// });
+document.addEventListener("DOMContentLoaded", () => {
+    //Declare variables for the elements of the html 
+    const icon = document.getElementById("icon");
+    const iconTwo = document.getElementById("icontwo");
+    const ul = document.querySelector("ul");
+    //condition to check if the icon,icontwo and ul exist
+    if (icon && iconTwo && ul) {
+        //When fa-bars is clicked it adds the show class,then it's replaced with the fa-times
+      icon.addEventListener("click", () => {
+        ul.classList.add("show");
+        icon.style.display = "none";
+        iconTwo.style.display = "block";
+      });
+      //when fa-times is clicked it removes the .show class, then it's display is replaced with the fa-bars
+      iconTwo.addEventListener("click", () => {
+        ul.classList.remove("show");
+        icon.style.display = "block";
+        iconTwo.style.display = "none";
+      });
+    }
+  });
+  
