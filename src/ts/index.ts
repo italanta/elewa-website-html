2887c3a88ce1c1a794960ec8210f0dc965380ba8
tugi <blueqwinn@gmail.com>
2023-10-10 13:49:17 +0300

    :memo:  typescript has been added in my   index.ts

---
+++ src/ts/index.ts
@@ -0,0 +1,11 @@
+const toggleBtn: HTMLElement | null = document.querySelector('.toggle_btn');
+const toggleBtnIcon: HTMLElement | null = document.querySelector('.toggle_btn i');
+const dropDownMenu: HTMLElement | null = document.querySelector('.navbar_dropdown_menu');
+
+if (toggleBtn && toggleBtnIcon && dropDownMenu) {
+  toggleBtn.onclick = function () {
+    dropDownMenu.classList.toggle('open');
+    const isOpen: boolean = dropDownMenu.classList.contains('open');
+    toggleBtnIcon.className = isOpen ? 'fa-solid fa-xmark' : 'fa-solid fa-bars';
+  };
+}
