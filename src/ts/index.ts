c9cdb43b8689f6d66be0aaadf314d05bb307e162
tugi <blueqwinn@gmail.com>
2023-10-14 13:45:52 +0300

    :fire: removed a file

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,4 +1,4 @@
-content-development-page-hero-section
+/**content-development-page-hero-section */
 const toggleBtn: HTMLElement | null = document.querySelector('.toggle_btn');
 const toggleBtnIcon: HTMLElement | null = document.querySelector('.toggle_btn i');
 const dropDownMenu: HTMLElement | null = document.querySelector('.navbar_dropdown_menu');
@@ -11,26 +11,3 @@ if (toggleBtn && toggleBtnIcon && dropDownMenu) {
   };
 }
 
-document.addEventListener("DOMContentLoaded", () => {
-    //Declare variables for the elements of the html 
-    const icon = document.getElementById("icon");
-    const iconTwo = document.getElementById("icontwo");
-    const ul = document.querySelector("ul");
-    //condition to check if the icon,icontwo and ul exist
-    if (icon && iconTwo && ul) {
-        //When fa-bars is clicked it adds the show class,then it's replaced with the fa-times
-      icon.addEventListener("click", () => {
-        ul.classList.add("show");
-        icon.style.display = "none";
-        iconTwo.style.display = "block";
-      });
-      //when fa-times is clicked it removes the .show class, then it's display is replaced with the fa-bars
-      iconTwo.addEventListener("click", () => {
-        ul.classList.remove("show");
-        icon.style.display = "block";
-        iconTwo.style.display = "none";
-      });
-    }
-  });
-  
-main
