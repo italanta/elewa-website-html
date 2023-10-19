d4bdd5be208141022dc51324fd825bb573a70578
Brian <mahuibrian14@gmail.com>
2023-10-19 12:01:06 +0300

    🚧: list loop functionality added

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -3,18 +3,39 @@
 // Sample card data (replace with your own data)
 const cardData: Card[] = [
     {
-      imageUrl: 'https://your-image-url1.jpg',
+      imageUrl: 'https://res.cloudinary.com/dyl3rncv3/image/upload/v1679665800/elewa-group-website/elewa-team-members/jeff-elewa_hdupp7.jpg',
       imageAlt: 'Image 1',
       buttonText: 'Learn More',
       cardTitle: 'Card 1',
       cardDescription: 'Description for Card 1',
     },
     {
-      imageUrl: 'https://your-image-url2.jpg',
+      imageUrl: 'https://res.cloudinary.com/dyl3rncv3/image/upload/c_scale,h_1000/v1682586211/IMG_9755_vfprhm.jpg',
       imageAlt: 'Image 2',
       buttonText: 'Learn More',
       cardTitle: 'Card 2',
       cardDescription: 'Description for Card 2',
+    },
+    {
+        imageUrl: 'https://res.cloudinary.com/dyl3rncv3/image/upload/v1679656820/elewa-group-website/elewa-team-members/frank-elewa_kihn9e.jpg',
+        imageAlt: 'Image 3',
+        buttonText: 'Learn More',
+        cardTitle: 'Card 3',
+        cardDescription: 'Description for Card 2',
+    },
+    {
+        imageUrl: 'https://res.cloudinary.com/dyl3rncv3/image/upload/v1679656820/elewa-group-website/elewa-team-members/jente-elewa_wbqyyi.jpg',
+        imageAlt: 'Image 4',
+        buttonText: 'Learn More',
+        cardTitle: 'Card 4',
+        cardDescription: 'Description for Card 2',
+    },
+    {
+        imageUrl: 'https://res.cloudinary.com/dyl3rncv3/image/upload/v1679656822/elewa-group-website/elewa-team-members/ken-elewa_ydwzop.jpg',
+        imageAlt: 'Image 5',
+        buttonText: 'Learn More',
+        cardTitle: 'Card 5',
+        cardDescription: 'Description for Card 2',
     },
     // Add more card data objects as needed
   ];
@@ -64,8 +85,11 @@ const cardData: Card[] = [
   scrollLeftBtn.addEventListener('click', () => {
     if (currentOffset > 0) {
       currentOffset -= 1;
-      updateCarousel();
+    } else {
+      // If the current card is the first one, set the offset to the last card
+      currentOffset = Math.max(0, cardContainer.children.length - 4);
     }
+    updateCarousel();
   });
   
   // Event listener for the "Scroll Right" button
@@ -73,10 +97,12 @@ const cardData: Card[] = [
     const maxOffset = Math.max(0, cardContainer.children.length - 4);
     if (currentOffset < maxOffset) {
       currentOffset += 1;
-      updateCarousel();
+    } else {
+      // If the current card is the last one, set the offset to 0 (first card)
+      currentOffset = 0;
     }
+    updateCarousel();
   });
-  
   // Function to update the position of the card carousel
   function updateCarousel(): void {
     cardContainer.style.transform = `translateX(${-currentOffset * cardWidth}px)`;
