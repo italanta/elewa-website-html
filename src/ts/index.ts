e7efbf208576b68bd34037dc2690c146ec7264a0
Brian <mahuibrian14@gmail.com>
2023-10-19 10:23:34 +0300

    🚧:scroll button functionality  working

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,14 +1,30 @@
-const prevBtn= document.querySelector("#scroll-left");
-const nextBtn= document.querySelector("#scroll-right");
-const carouselSlide=document.querySelector("#carousel-list");
-const carouselCards=document.querySelector(".card");
+const carouselList = document.getElementById('carousel-list');
+const scrollLeftBtn = document.getElementById('scroll-left');
+const scrollRightBtn = document.getElementById('scroll-right');
+const cardWidth = 474; // Adjust this value according to your card width in pixels
+const maxVisibleCards = 4;
+let currentOffset = 0;
 
-let counter: number=1;
-/*carouselCards is an array of cards having data like images,description etc*/
-//const size= carouselCards[0].clientWidth;
-//carouselSlide.style.transform='translateX(' + (-size * counter) + 'px)';
- //button listeners
+// Function to update the position of the card carousel
+function updateCarousel() {
+  carouselList.style.transform = `translateX(${-currentOffset * cardWidth}px)`;
+}
 
-nextBtn.addEventListener('click',()=>{
-    console.log("nextBtn clicked")
-})
+// Event listener for the "Scroll Left" button
+scrollLeftBtn.addEventListener('click', () => {
+  if (currentOffset > 0) {
+    currentOffset -= 1;
+    updateCarousel();
+  }
+});
+
+// Event listener for the "Scroll Right" button
+scrollRightBtn.addEventListener('click', () => {
+  if (currentOffset < Math.ceil(carouselList.children.length / maxVisibleCards) - 1) {
+    currentOffset += 1;
+    updateCarousel();
+  }
+});
+
+// Initial position of the carousel
+updateCarousel();
