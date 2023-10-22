e4b5ba8a05522c4a16db6f4e38856328513003d9
stephen <skaranja654@gmail.com>
2023-10-22 12:23:57 +0300

    :memo: added the JSDoc comment for the interface

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,5 +1,9 @@
 // defining the interface objects for the projectItems
+// JSDoc comment
 
+/**
+ * Interface for classes that represent a project.
+ */
 interface ProjectItem {
   title: string;
   description: string;
@@ -9,10 +13,39 @@ interface ProjectItem {
 
 // This is the data contained in the project one as displayed on the html
 
+// project one
 const projectOne: ProjectItem = {
   title: "Introducing conversational learning",
   description: "Eos qui ratione voluptatem sequi nesciunt.Neque porro quisquam dolorem ipsum quia",
   tag: "Education",
   imgSrc: "/src/images/Education.jpg"
 };
+console.log(projectOne)
 
+// project two
+const projectTwo: ProjectItem = {
+  title: "Content Development for a sustainable future",
+  description: "Eos qui ratione voluptatem sequi nesciunt.Neque porro quisquam dolorem ipsum quia",
+  tag: "Environment",
+  imgSrc: "/src/images/Environment.jpg"
+};
+console.log(projectTwo)
+
+// project three
+const projectThree: ProjectItem = {
+  title: "Largescale training for healthcare professionals",
+  description: "Eos qui ratione voluptatem sequi nesciunt.Neque porro quisquam dolorem ipsum quia",
+  tag: "Healthcare",
+  imgSrc: "/src/images/Healthcare.jpg"
+};
+console.log(projectThree)
+
+// project four
+const projectFour: ProjectItem = {
+  title: "Introducing conversational learning",
+  description: "Eos qui ratione voluptatem sequi nesciunt.Neque porro quisquam dolorem ipsum quia",
+  tag: "Education",
+  imgSrc: "/src/images/Learning.jpg"
+};
+
+console.log(projectFour)
