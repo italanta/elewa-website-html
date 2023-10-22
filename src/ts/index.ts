980960249d033af6365233f950def984b2df6409
stephen <skaranja654@gmail.com>
2023-10-22 12:14:23 +0300

    :memo: added the typescript interface that has the data layout for the projects

---
+++ src/ts/index.ts
@@ -0,0 +1,18 @@
+// defining the interface objects for the projectItems
+
+interface ProjectItem {
+  title: string;
+  description: string;
+  tag: string;
+  imgSrc: string;
+}
+
+// This is the data contained in the project one as displayed on the html
+
+const projectOne: ProjectItem = {
+  title: "Introducing conversational learning",
+  description: "Eos qui ratione voluptatem sequi nesciunt.Neque porro quisquam dolorem ipsum quia",
+  tag: "Education",
+  imgSrc: "/src/images/Education.jpg"
+};
+
