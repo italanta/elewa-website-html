9d4099e57a827b6e288567ab755fb758667089da
[sharoun <[madoyasharoun@gmail.com]>
2023-10-23 16:20:12 +0300

    :bulb: Javascipt slider functionality

---
+++ src/ts/index.js
@@ -0,0 +1,30 @@
+const slides = document.querySelectorAll('.slide');
+let index = 0;
+
+function prevSlide(){
+    slides[index].classList.remove('active');
+    index--;
+
+    if(index < 0)
+        index = slides.length -1;
+
+    slides[index].classList.add('active');      
+}
+
+document.querySelector('.bottom-btn1').addEventListener('click', e => {
+    prevSlide();
+});
+
+function nextSlide(){
+    slides[index].classList.remove('active');
+    index++;
+
+    if(index > slides.length -1)
+        index = 0;
+
+    slides[index].classList.add('active');      
+}
+
+document.querySelector('.bottom-btn2').addEventListener('click', e => {
+    nextSlide();
+});
