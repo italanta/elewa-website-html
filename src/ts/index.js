13decf601336f3f80cc85b5e4859a0815a51ee40
[sharoun <[madoyasharoun@gmail.com]>
2023-10-24 12:06:25 +0300

    :memo: added typescript dependencies

--- src/ts/index.js
+++ src/ts/index.js
@@ -2,29 +2,29 @@ const slides = document.querySelectorAll('.slide');
 let index = 0;
 
 function prevSlide(){
-    slides[index].classList.remove('active');
-    index--;
+slides[index].classList.remove('active');
+index--;
 
-    if(index < 0)
-        index = slides.length -1;
+if(index < 0)
+index = slides.length -1;
 
-    slides[index].classList.add('active');      
+slides[index].classList.add('active'); 
 }
 
 document.querySelector('.bottom-btn1').addEventListener('click', e => {
-    prevSlide();
+prevSlide();
 });
 
 function nextSlide(){
-    slides[index].classList.remove('active');
-    index++;
+slides[index].classList.remove('active');
+index++;
 
-    if(index > slides.length -1)
-        index = 0;
+if(index > slides.length -1)
+index = 0;
 
-    slides[index].classList.add('active');      
+slides[index].classList.add('active'); 
 }
 
 document.querySelector('.bottom-btn2').addEventListener('click', e => {
-    nextSlide();
+nextSlide();
 });
