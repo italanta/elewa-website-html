99f7dc0425a1d830476e4e53906b1c27d53bde93
[sharoun <[madoyasharoun@gmail.com]>
2023-10-26 11:08:39 +0300

    :boom: typescript changed errors

--- src/ts/index.js
+++ src/ts/index.js
@@ -29,35 +29,20 @@ var projectFour = {
     tag: "Education",
     imgSrc: "/assets/images/tree me (1).jpg"
 };
-// function that allows the carousel to respond
-var prevButton = document.querySelector("bottom-btn1"); // targeting the buttons
-var nextButton = document.querySelector("bottom-btn2");
-var slides = document.querySelectorAll(".card"); // targeting the projects container. The HTMLElements helps in making the style a property of the container.
-var slideContainer = document.querySelector("card-container");
+// Get references to the elements
+var prevButton = document.querySelector(".bottom-btn1"); // Targeting the buttons
+var nextButton = document.querySelector(".bottom-btn2");
+var slides = document.querySelectorAll(".card"); // Targeting the projects container
+var slideContainer = document.querySelector(".card-container");
 var index = 0;
-// console.log(slides)
 function showSlide(index) {
     for (var i = 0; i < slides.length; i++) {
-        console.log(slides[i]);
         slides[i].style.opacity = '0.3';
         slides[i].classList.remove('active');
     }
     slides[index].style.opacity = '1';
     slides[index].classList.add('active');
-    if (slideContainer) {
-        slideContainer.style.transform = "translateX(-$(index * 100)%)";
-    }
 }
-// for(let i = 0; i < slides.length; i++) {
-//   console.log(slides[i]);
-//   slides[i].style.opacity= '0.3';
-//   slides[i].classList.remove('active');
-// }
-// slides[index].style.opacity='1';
-// slides[index].classList.add('active');
-// if (slideContainer) {
-//   slideContainer.style.transform = `translateX(-$(index * 100)%)`;
-// }
 prevButton === null || prevButton === void 0 ? void 0 : prevButton.addEventListener('click', function () {
     index--;
     if (index < 0) {
@@ -73,34 +58,3 @@ nextButton === null || nextButton === void 0 ? void 0 : nextButton.addEventListe
     showSlide(index);
 });
 showSlide(index);
-// prevButton?.addEventListener("click", function handleClick (event) {
-//   console.log("clicked")
-//   slides.style.transition = "transform 0.5s";
-//   slides.style.transform = "translateX(-200px)";
-// });
-// nextButton?.addEventListener("click", function handleClick (event) {
-//   console.log("clicked")
-//   slides.style.transition = "transform 0.5s";
-//   slides.style.transform = "translateX(200px)";
-// });
-// document.addEventListener("DOMContentLoaded", () => {
-//     //Declare variables for the elements of the html 
-//     const icon = document.querySelector("icon");
-//     const iconTwo = document.querySelector("icontwo");
-//     const ul = document.querySelector("ul");
-//     //condition to check if the icon,icontwo and ul exist
-//     if (icon && iconTwo && ul) {
-//         //When fa-bars is clicked it adds the show class,then it's replaced with the fa-times
-//       icon.addEventListener("click", () => {
-//         ul.classList.add("show");
-//         icon.style.display = "none";
-//         iconTwo.style.display = "block";
-//       });
-//       //when fa-times is clicked it removes the .show class, then it's display is replaced with the fa-bars
-//       iconTwo.addEventListener("click", () => {
-//         ul.classList.remove("show");
-//         icon.style.display = "block";
-//         iconTwo.style.display = "none";
-//       });
-//     }
-//   });
--- src/ts/index.ts
+++ src/ts/index.ts
@@ -46,98 +46,38 @@ interface ProjectItem {
     imgSrc: "/assets/images/tree me (1).jpg"
   };
 
-  
-  // function that allows the carousel to respond
-  const prevButton = document.querySelector<HTMLButtonElement>("bottom-btn1"); // targeting the buttons
-  const nextButton = document.querySelector<HTMLButtonElement>("bottom-btn2");
-  const slides = document.querySelectorAll<HTMLDivElement>(".card"); // targeting the projects container. The HTMLElements helps in making the style a property of the container.
-  const slideContainer = document.querySelector<HTMLDivElement>("card-container")
-  let index: number = 0;
-  // console.log(slides)
+ // Get references to the elements
+const prevButton = document.querySelector<HTMLButtonElement>(".bottom-btn1"); // Targeting the buttons
+const nextButton = document.querySelector<HTMLButtonElement>(".bottom-btn2");
+const slides = document.querySelectorAll<HTMLDivElement>(".card"); // Targeting the projects container
+const slideContainer = document.querySelector<HTMLDivElement>(".card-container");
+let index: number = 0;
 
-
-  function showSlide(index : number) {
-    for(let i = 0; i < slides.length; i++) {
-      console.log(slides[i]);
-      slides[i].style.opacity= '0.3';
-      slides[i].classList.remove('active');
-    }
-    slides[index].style.opacity='1';
-    slides[index].classList.add('active');
-    if (slideContainer) {
-      slideContainer.style.transform = `translateX(-$(index * 100)%)`;
-    }
+function showSlide(index: number) {
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.opacity = '0.3';
+    slides[i].classList.remove('active');
+  }
+  slides[index].style.opacity = '1';
+  slides[index].classList.add('active');
   }
 
-  // for(let i = 0; i < slides.length; i++) {
-  //   console.log(slides[i]);
-  //   slides[i].style.opacity= '0.3';
-  //   slides[i].classList.remove('active');
-  // }
-  // slides[index].style.opacity='1';
-  // slides[index].classList.add('active');
-  // if (slideContainer) {
-  //   slideContainer.style.transform = `translateX(-$(index * 100)%)`;
-  // }
 
-prevButton?.addEventListener('click',()=>{
+prevButton?.addEventListener('click', () => {
   index--;
-  if(index < 0){
-    index=slides.length-1;
+  if (index < 0) {
+    index = slides.length - 1;
   }
   showSlide(index);
-})
+});
 
-nextButton?.addEventListener('click',()=>{
+nextButton?.addEventListener('click', () => {
   index++;
-  if(index >= slides.length){
-    index= 0;
+  if (index >= slides.length) {
+    index = 0;
   }
   showSlide(index);
-})
+});
 
-showSlide(index)
+showSlide(index);
 
-
-
-
-
-
-
-
-
-
-
-
-  // prevButton?.addEventListener("click", function handleClick (event) {
-  //   console.log("clicked")
-  //   slides.style.transition = "transform 0.5s";
-  //   slides.style.transform = "translateX(-200px)";
-  // });
-  
-  // nextButton?.addEventListener("click", function handleClick (event) {
-  //   console.log("clicked")
-  //   slides.style.transition = "transform 0.5s";
-  //   slides.style.transform = "translateX(200px)";
-  // });
-  // document.addEventListener("DOMContentLoaded", () => {
-  //     //Declare variables for the elements of the html 
-  //     const icon = document.querySelector("icon");
-  //     const iconTwo = document.querySelector("icontwo");
-  //     const ul = document.querySelector("ul");
-  //     //condition to check if the icon,icontwo and ul exist
-  //     if (icon && iconTwo && ul) {
-  //         //When fa-bars is clicked it adds the show class,then it's replaced with the fa-times
-  //       icon.addEventListener("click", () => {
-  //         ul.classList.add("show");
-  //         icon.style.display = "none";
-  //         iconTwo.style.display = "block";
-  //       });
-  //       //when fa-times is clicked it removes the .show class, then it's display is replaced with the fa-bars
-  //       iconTwo.addEventListener("click", () => {
-  //         ul.classList.remove("show");
-  //         icon.style.display = "block";
-  //         iconTwo.style.display = "none";
-  //       });
-  //     }
-  //   });
